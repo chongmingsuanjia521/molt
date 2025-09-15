46576f4e5529653b03f8d62e63edd1a98cb3fd63
chongmingsuanjia <Wjm3334259163@outlook.com>
2025-09-15 19:36:07 +0800

    用户增删改查

--- back-end/molt-server/src/main/java/asia/wjm/mapper/UserMapper.java
+++ back-end/molt-server/src/main/java/asia/wjm/mapper/UserMapper.java
@@ -3,7 +3,9 @@ package asia.wjm.mapper;
 import asia.wjm.DTO.UserPageQueryDTO;
 import asia.wjm.entity.User;
 import com.github.pagehelper.Page;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -12,5 +14,16 @@ public interface UserMapper {
 
 
     List<User> pageQuery(UserPageQueryDTO userPageQueryDTO);
+
+    void insert(User user);
+
+    @Select("SELECT * FROM user WHERE id = #{id}")
+    User getById(Long id);
+
+    List<User> getByIds(List<Long> ids);
+
+    void deleteBatch(List<Long> ids);
+
+    void update(User user);
 }
 
